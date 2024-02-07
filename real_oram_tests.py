f379f632d5068310aa29765d0fce4a8e61396029
Drscq <128768604+Drscq@users.noreply.github.com>
2024-02-07 10:52:13 -0500

    Update real_oram_tests.py
    Delete the interaction with the user

--- real_oram_tests.py
+++ real_oram_tests.py
@@ -2,8 +2,8 @@
 from real_oram.ORAM import ORAM
 from real_oram.RAM.local_RAM import local_RAM, reset_counters
 from loading_bar import update_loading_bar
-
-    
+from real_oram.config import config
+import time
 #our ORAM test
 def _real_oram_test(oram_size):
 
@@ -12,25 +12,37 @@ def _real_oram_test(oram_size):
     # allocating memory shouldn't count as 'writing'...
     reset_counters()
     oram.initial_build('testing_data')
+    start_time = time.time()
     for i in range(int(oram_size)-1):
         oram.access('write',int(i).to_bytes(oram.conf.KEY_SIZE,'big'),int(i+3).to_bytes(oram.conf.BALL_DATA_SIZE,'big'))
         if i % 10_000 == 0:
             update_loading_bar(i/oram_size)
+    end_time = time.time()
+    total_time = end_time - start_time
+    # print the total time in nanoseconds
+    print('Total time:', total_time, 'seconds')
+    print('Average time per access:', total_time/oram_size, 'seconds')
+    print('Average time per access:', total_time/oram_size*10**9, 'nanoseconds')
+    # average time per access divided by oram_size
+    print('Average time per access:', total_time/oram_size, 'seconds')
+    print('Average time per access:', total_time/oram_size*10**9, 'nanoseconds')
 
 
-def real_oram_test():
-    number_of_MB = int(input('How many MB of storage should the test allocate?\n'))
+def real_oram_test(numberOfBlocks):
+    # number_of_MB = int(input('How many MB of storage should the test allocate?\n'))
 
     # The amount of data in each block is 16 bytes
-    number_of_blocks = int((number_of_MB*(2**20))/16)
-    
+    # number_of_blocks = int((number_of_MB*(2**20))/16)
+    # number_of_blocks = int((number_of_MB*(2**20))/config.BALL_DATA_SIZE)
+    number_of_blocks = numberOfBlocks
+    number_of_MB = number_of_blocks*config.BALL_DATA_SIZE/(2**20)
     print('Executing',number_of_blocks,'accesses (the size of the ORAM as every block contains 16bytes of data)')
     if number_of_MB > 50:
         print('Due to the initial build it might take several minutes before accesses begin.')
     
     if number_of_MB > 1000:
         print('Due to the initial build it might take several hours before accesses begin.')
-
+    
     _real_oram_test(number_of_blocks)
     
     print('\naccesses: ', number_of_blocks) 
